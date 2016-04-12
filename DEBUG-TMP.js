b744d0dd35cee877441df5a5b27371ec3cc7abd3
Thomas Tortorini <thomastortorini@gmail.com>
2016-04-12 16:46:08 +0200

    UI: prepare the .audionode 's CSS

--- DEBUG-TMP.js
+++ DEBUG-TMP.js
@@ -20,11 +20,10 @@ ui.tracks[ 3 ].toggle( false ).editName( "is in an" );
 ui.tracks[ 4 ].toggle( false ).editName( "active rush!" );
 ui.tracks[ 7 ].editName( "github.com/GridSound/gridsound.github.io" );
 ui.tracks[ 9 ].toggle( false );
-ui.jqTrackLines.children().eq(0).append("<div class='audionode' style='left:1em'>")
-ui.jqTrackLines.children().eq(1).append("<div class='audionode' style='left:0em'>")
-ui.jqTrackLines.children().eq(2).append("<div class='audionode' style='left:10em'>")
-ui.jqTrackLines.children().eq(3).append("<div class='audionode' style='left:0em'>")
-ui.jqTrackLines.children().eq(4).append("<div class='audionode' style='left:7em'>")
-ui.jqTrackLines.children().eq(5).append("<div class='audionode' style='left:8em'>")
-ui.jqTrackLines.children().eq(6).append("<div class='audionode' style='left:9em'>")
-ui.jqTrackLines.children().eq(7).append("<div class='audionode' style='left:7em'>")
+
+ui.jqTrackLines.children().eq(1).append(
+	"<div class='audionode' style='left:2em; width:4em;'>" +
+		"<div class='waveform'></div>" +
+		"<div class='title text-overflow'>Drum_p55</div>" +
+	"</div>"
+);
