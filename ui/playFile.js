1dcd6abccead5dfed8b764f7986be49437b0d5d0
Thomas Tortorini <thomastortorini@gmail.com>
2016-04-12 23:42:45 +0200

    UI: #files a .cursor to indicate the currentTime of the buffer

--- ui/playFile.js
+++ ui/playFile.js
@@ -2,17 +2,29 @@
 
 (function() {
 
-var wsample;
+var
+	wsample,
+	jqCursor = $( "<div class='cursor'>" )
+;
 
 ui.playFile = function( uifile ) {
 	if ( wsample ) {
 		wsample.stop();
 	}
-	wsample = uifile.wbuff.createSample().load().start();
+	if ( uifile.isLoaded ) {
+		jqCursor.css( "transitionDuration", 0 ).css( "left", 0 );
+		uifile.jqCanvasWaveform.after( jqCursor );
+		wsample = uifile.wbuff.createSample().load().start();
+		setTimeout( function() {
+			jqCursor.css( "transitionDuration", uifile.wbuff.buffer.duration + "s" )
+				.css( "left", "100%" );
+		}, 20 );
+	}
 };
 
 ui.stopFile = function() {
 	wsample.stop();
+	jqCursor.detach();
 };
 
 })();
