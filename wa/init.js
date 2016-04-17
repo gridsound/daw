8371228b5b6027571117801760041abaadf78fc2
Thomas Tortorini <thomastortorini@gmail.com>
2016-04-17 12:42:39 +0200

    WA: sync with library (filters)

--- wa/init.js
+++ wa/init.js
@@ -4,6 +4,8 @@ window.wa = {};
 
 wa.wctx = new walContext();
 wa.ctx = wa.wctx.ctx;
-wa.analyser = wa.wctx.analyser;
+
+wa.analyser = wa.ctx.createAnalyser();
 wa.analyser.fftSize = 1024;
+wa.wctx.filters.pushBack( wa.analyser );
 wa.analyserArray = new Uint8Array( wa.analyser.frequencyBinCount );
