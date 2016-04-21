f28a76f3eed682bae694430cc48b337295da177d
Thomas Tortorini <thomastortorini@gmail.com>
2016-04-21 02:29:55 +0200

    UI: zoom with ctrlKey instead of spaceKey

--- ui/events/gridWheel.js
+++ ui/events/gridWheel.js
@@ -8,16 +8,16 @@ ui.jqBody.on( "wheel", function( e ) {
 
 ui.jqGrid.on( "wheel", function( e ) {
 	e = e.originalEvent;
-	if ( ui.currentTool !== "hand" ) {
-		// Vertical scroll:
-		ui.setGridTop( ui.gridTop + ( e.deltaY < 0 ? .9 : -.9 ) * ui.gridEm );
-	} else {
+	if ( e.ctrlKey ) {
 		// Zoom:
 		ui.setGridZoom(
 			ui.gridZoom * ( e.deltaY < 0 ? 1.1 : 0.9 ),
 			e.pageX - ui.filesWidth - ui.trackNamesWidth,
 			e.pageY - ui.gridColsY
 		);
+	} else {
+		// Vertical scroll:
+		ui.setGridTop( ui.gridTop + ( e.deltaY < 0 ? .9 : -.9 ) * ui.gridEm );
 	}
 	ui.updateGridBoxShadow();
 });
