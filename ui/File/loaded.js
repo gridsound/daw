644a66edfdb18c0c24a1b908e21cd1f2ccf3390a
Misty418 <melanieducani@gmail.com>
2016-04-29 10:04:25 +0200

    WA: sync with library (waveform)

--- ui/File/loaded.js
+++ ui/File/loaded.js
@@ -8,16 +8,22 @@ ui.File.prototype.loaded = function() {
 		.addClass( "fa-refresh fa-spin" );
 
 	wa.wctx.createBuffer( this.file, function( wbuff ) {
+		var canvas, ctx, img;
+
 		that.wbuff = wbuff;
 		that.isLoaded = true;
 		that.isLoading = false;
 		that.jqFile.removeClass( "to-load" );
 		that.jqToLoad.remove();
 		that.jqCanvasWaveform = $( "<canvas class='waveform'>" );
-		var canvas = that.jqCanvasWaveform[ 0 ];
+		canvas = that.jqCanvasWaveform[ 0 ];
+		ctx = canvas.getContext( "2d" );
 		canvas.width = 400;
 		canvas.height = 50;
-		that.jqFile.prepend( wbuff.drawWaveform( canvas, [ 0x39, 0x39, 0x5A, 0xFF ] ) );
+		img = ctx.createImageData( canvas.width, canvas.height );
+		wbuff.drawWaveform( img, [ 0x39, 0x39, 0x5A, 0xFF ] )
+		ctx.putImageData( img, 0, 0 );
+		that.jqFile.prepend( canvas );
 		ui.playFile( that );
 	});
 };
--- ui/Sample/constructor.js
+++ ui/Sample/constructor.js
@@ -1,19 +1,23 @@
 "use strict";
 
 ui.Sample = function( uifile ) {
+	var canvas, ctx, img;
+
 	this.uifile = uifile;
 	this.wbuff = uifile.wbuff;
-
 	this.offsetEm = 0;
 
 	this.jqSample = $( "<div class='sample'>" );
 	this.jqWaveformWrapper = $( "<div class='waveformWrapper'>" )
 		.appendTo( this.jqSample );
 	this.jqWaveform = $( "<canvas class='waveform'>" ).appendTo( this.jqWaveformWrapper );
-	var canvas = this.jqWaveform[ 0 ];
+	canvas = this.jqWaveform[ 0 ];
+	ctx = canvas.getContext( "2d" );
 	canvas.width = ~~( this.wbuff.buffer.duration * 300 );
 	canvas.height = 50;
-	this.wbuff.drawWaveform( canvas, [ 0xDD, 0xDD, 0xFF, 0xFF ] );
+	img = ctx.createImageData( canvas.width, canvas.height );
+	this.wbuff.drawWaveform( img, [ 0xDD, 0xDD, 0xFF, 0xFF ] );
+	ctx.putImageData( img, 0, 0 );
 	this.jqName = $( "<span class='text-overflow'>" )
 		.text( uifile.name ).appendTo( this.jqSample );
 
