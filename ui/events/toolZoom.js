a7a30411c5fda6f8785b254a0cca96486e46e600
Thomas Tortorini <thomastortorini@gmail.com>
2016-05-04 00:23:48 +0200

    UI: ui.tool.hand -> mousedown

--- ui/events/toolZoom.js
+++ ui/events/toolZoom.js
@@ -1,11 +1,24 @@
 "use strict";
 
+(function() {
+
+function zoom( e, z ) {
+	ui.setGridZoom(
+		ui.gridZoom * z,
+		e.pageX - ui.filesWidth - ui.trackNamesWidth,
+		e.pageY - ui.gridColsY
+	);
+}
+
 ui.tool.zoom = {
 	wheel: function( e ) {
-		ui.setGridZoom(
-			ui.gridZoom * ( e.deltaY < 0 ? 1.1 : 0.9 ),
-			e.pageX - ui.filesWidth - ui.trackNamesWidth,
-			e.pageY - ui.gridColsY
-		);
+		zoom( e, e.deltaY < 0 ? 1.1 : .9 );
+	},
+	mousedown: function( e ) {
+		if ( e.button === 0 ) {
+			zoom( e, e.altKey ? .8 : 1.2 );
+		}
 	}
 };
+
+})();
