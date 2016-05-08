cabbceb3da13c4775d13ad7cd3b25df785d4f985
Thomas Tortorini <thomastortorini@gmail.com>
2016-05-08 20:46:46 +0200

    UI: no more need of cloneCanvas

--- jstools/cloneCanvas.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function cloneCanvas( canvas ) {
-	var newc = document.createElement( "canvas" );
-	newc.width = canvas.width;
-	newc.height = canvas.height;
-	newc.getContext( "2d" ).drawImage( canvas, 0, 0 );
-	return newc;
-}
