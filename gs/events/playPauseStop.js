26b23975326a606c42c771c4ce58334dcdd8d2bb
Thomas Tortorini <thomastortorini@gmail.com>
2016-05-31 14:14:15 +0200

    GS: play() -> stopFile(), stop the sample preview when we start the composition

--- gs/events/playPauseStop.js
+++ gs/events/playPauseStop.js
@@ -1,10 +1,13 @@
 "use strict";
 
-ui.jqPlay.click( gs.playToggle );
+ui.jqPlay.click( function() {
+	ui.stopFile();
+	gs.playToggle();
+} );
 
 ui.jqStop.click( function() {
 	ui.stopFile();
 	gs.stop();
-});
+} );
 
 wa.composition.onended( ui.stop );
