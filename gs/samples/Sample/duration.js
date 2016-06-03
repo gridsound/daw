8e0ab96d0a38780873cefe4792367b02d34ceeb2
Thomas Tortorini <thomastortorini@gmail.com>
2016-06-03 14:04:46 +0200

    GS: Sample.duration(), can't increase the duration more than its bufferDuration

--- gs/samples/Sample/duration.js
+++ gs/samples/Sample/duration.js
@@ -1,7 +1,6 @@
 "use strict";
 
 gs.Sample.prototype.duration = function( dur ) {
-	// this.wsample.bufferDuration ?
-	this.wsample.duration = Math.max( 0, dur );
+	this.wsample.duration = Math.max( 0, Math.min( dur, this.wsample.bufferDuration ) );
 	ui.CSS_sampleDuration( this );
 };
--- gs/samples/samplesDuration.js
+++ gs/samples/samplesDuration.js
@@ -3,12 +3,15 @@
 gs.samplesDuration = function( sample, mxem ) {
 	mxem /= ui.BPMem;
 
-	if ( sample.selected && mxem < 0 ) {
+	if ( sample.selected ) {
 		var durMin = Infinity;
 		gs.selectedSamples.forEach( function( s ) {
 			durMin = Math.min( durMin, s.wsample.duration );
+			mxem = Math.min( mxem, s.wsample.bufferDuration - s.wsample.duration );
 		});
-		mxem = -Math.min( durMin, -mxem );
+		if ( mxem < 0 ) {
+			mxem = -Math.min( durMin, -mxem );
+		}
 	}
 
 	gs.samplesForEach( sample, function( s ) {
