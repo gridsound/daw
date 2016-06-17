991455b338fdb54f54e427eef89ae77a421f2bd7
Melanie Misty Ducani <melanieducani@gmail.com>
2016-06-17 18:14:08 +0200

    clean

--- gs/save.js
+++ gs/save.js
@@ -1,24 +1,20 @@
 "use strict"
 
-gs.save = function()
-{
-	var
-		_save = {
+gs.save = function() {
+	var _save = {
 			bpm: this._bpm,
 			files: [],
 			samples: [],
 			tracks: []
-		}
-	;
+		};
 
 	ui.files.forEach( function( f ) {
 		_save.files.push( [
 			f.id,
 			f.file.name,
-			f.file.size,
-			f.file.type
-		]);
-	});
+			f.file.size
+		] );
+	} );
 
 	gs.samples.forEach( function( s ) {
 		_save.samples.push( [
@@ -28,8 +24,8 @@ gs.save = function()
 			s.wsample.when,
 			s.wsample.offset,
 			s.wsample.duration
-		]);
-	});
+		] );
+	} );
 
 	// TODO save wfilters
 	ui.tracks.forEach( function( t ) {
@@ -39,12 +35,12 @@ gs.save = function()
 				t.id,
 				t.isOn,
 				t.name
-			]);
+			] );
 		}
-	});
+	} );
 
 	return {
-		href: "data:text/plain;charset=utf-8," + encodeURIComponent( JSON.stringify(_save) ),
+		href: "data:text/plain;charset=utf-8," + encodeURIComponent( JSON.stringify( _save ) ),
 		download: "s.txt"
 	};
 }
@@ -65,31 +61,27 @@ gs.load = function( file ) {
 		that._save.files.forEach( function( f ) {
 			nf = ui.newFile( f );
 			nf.samplesToSet = [];
-		});
+		} );
 
 		that._save.tracks.forEach( function( t ) {
-			var
-				id = t[ 0 ],
+			var id = t[ 0 ],
 				isOn = t[ 1 ],
-				name = t[ 2 ]
-			;
+				name = t[ 2 ];
 
 			while ( id >= ui.tracks.length ) {
 				ui.newTrack();
 			}
 			ui.tracks[ id ].isOn = isOn;
 			ui.tracks[ id ].name = name;
-		});
+		} );
 
 		that._save.samples.forEach( function( s ) {
-			var
-				xem = s[ 0 ],
+			var xem = s[ 0 ],
 				trackId = s[ 1 ],
 				fileId = s[ 2 ],
 				when = s[ 3 ],
 				offset = s[ 4 ],
-				duration = s[ 5 ]
-			;
+				duration = s[ 5 ];
 
 			ns = gs.sampleCreate( ui.files[ fileId ] );
 			ns.uifile.samplesToSet.push(ns);
@@ -110,7 +102,7 @@ gs.load = function( file ) {
 			// sample.duration()
 			ns.savedDuration = duration;
 			ns.jqSample.css( "width", duration * ui.BPMem + "em" );
-		});
+		} );
 	};
 	reader.readAsText( file );
-}
+};
