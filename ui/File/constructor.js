c335a45b84b9ac150d94c8a6dce2ebeef8516bd2
Thomas Tortorini <thomastortorini@gmail.com>
2016-06-19 16:07:13 +0200

    GS: File, then/catch around .decodeAudioData()

--- ui/File/constructor.js
+++ ui/File/constructor.js
@@ -5,34 +5,32 @@
 var clickedFile;
 
 ui.jqInputFile.change( function() {
-	if ( clickedFile )
-	{
+	if ( clickedFile ) {
 		clickedFile.joinFile( this.files[ 0 ] );
-		clickedFile = undefined;
+		clickedFile = null;
 	}
-});
-
+} );
 
 ui.File = function( file ) {
 	var that = this,
-		icone = file.length ? "question" : "download";
+		icon = file.length ? "question" : "download";
 
-	this.id = ui.files.length; // FIX ME when uifiles could be removed
-	this.file = file.length ? undefined : file;
-	this.fullname = file.name || file[ 1 ];
-	this.name = this.fullname.replace( /\.[^.]+$/, "" );
 	this.isLoaded =
 	this.isLoading = false;
+	this.id = ui.files.length; // FIXME when uifiles could be removed
+	this.file = file.length ? null : file;
+	this.fullname = file.name || file[ 1 ];
+	this.name = this.fullname.replace( /\.[^.]+$/, "" );
 	if ( !this.file ) {
 		this.savedSize = file[ 2 ];
 	}
 	this.jqFile = $( "<a class='sample to-load' draggable='true'>" );
-	this.jqToLoad = $( "<i class='to-load fa fa-fw fa-" + icone + "'>" );
+	this.jqToLoad = $( "<i class='to-load fa fa-fw fa-" + icon + "'>" );
 	this.jqName = $( "<span>" + this.name + "</span>" );
 	$( "<span class='name text-overflow'>" )
 		.append( this.jqToLoad )
 		.append( this.jqName )
-		.appendTo( this.jqFile )
+		.appendTo( this.jqFile );
 	this.jqFile.on( {
 		contextmenu: false,
 		dragstart: this.dragstart.bind( this ),
@@ -52,7 +50,7 @@ ui.File = function( file ) {
 				that.load( ui.playFile );
 			}
 		}
-	});
+	} );
 };
 
-})();
+} )();
--- ui/File/load.js
+++ ui/File/load.js
@@ -4,10 +4,10 @@ ui.File.prototype.load = function( fn ) {
 	var that = this;
 
 	this.isLoading = true;
-	this.jqToLoad.removeClass( "fa-downloads" )
+	this.jqToLoad.removeClass( "fa-download" )
 		.addClass( "fa-refresh fa-spin" );
 
-	wa.wctx.createBuffer( this.file, function( wbuff ) {
+	wa.wctx.createBuffer( this.file ).then( function( wbuff ) {
 		var canvas, ctx, img;
 
 		that.wbuff = wbuff;
@@ -25,5 +25,11 @@ ui.File.prototype.load = function( fn ) {
 		ctx.putImageData( img, 0, 0 );
 		that.jqFile.prepend( canvas );
 		fn( that );
-	});
+	}, function() {
+		that.isLoading = false;
+		that.jqToLoad.removeClass( "fa-refresh fa-spin" )
+			.addClass( "fa-times" );
+		alert( "At this day, the file: \"" + that.fullname +
+			"\" can not be decoded by your browser.\n" );
+	} );
 };
