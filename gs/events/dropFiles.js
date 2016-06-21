5f40e125c9a29d454efde95b8bb7197fa9fd1295
Melanie Misty Ducani <melanieducani@gmail.com>
2016-06-21 21:07:55 +0200

    GS : folders can be dropped, only in chrome.

--- gs/events/dropFiles.js
+++ gs/events/dropFiles.js
@@ -1,5 +1,58 @@
 "use strict";
 
+
+function extractData( droppedItems ) {
+	function traverseTree( item ) {
+		var p = new Promise( function( resolve ) {
+			if ( item.isFile ) {
+				item.file( function( file ) {
+					if ( file.type && file.type !== "text/plain" ) {
+						arrayFiles.push( file );
+					} else if ( !saveFile ) {
+						saveFile = file;
+						gs.reset();
+					}
+					resolve();
+				});
+			} else if ( item.isDirectory ) {
+				dirReader = item.createReader();
+				dirReader.readEntries( function( files ) {
+					var promArr = [];
+					$.each( files, function() {
+						promArr.push ( traverseTree( this ) );
+					} );
+					Promise.all( promArr )
+					.then( function() {
+						resolve();
+					} );
+				} );
+			}
+		} );
+		return p;
+	}
+
+	var
+		item,
+		dirReader,
+		saveFile,
+		arrayFiles = [],
+		arrayPromises = [];
+	;
+
+	$.each( droppedItems, function() {
+		if ( item = this.webkitGetAsEntry() ) {
+			arrayPromises.push ( traverseTree( item ) );
+		}
+	});
+	Promise.all( arrayPromises )
+		.then( function() {
+			gs.load( saveFile )
+			.then( function() {
+				loadFile( arrayFiles );
+			} );
+	} );
+}
+
 function loadFile( droppedFiles ) {
 	droppedFiles.forEach( function( file ) {
 		if ( !gs.files.some( function( f ) {
@@ -23,18 +76,28 @@ ui.jqBody.on( {
 		var data = e && e.dataTransfer,
 			saveFile = false,
 			droppedFiles = [];
-		$.each( data && data.files, function() {
-			if ( this.type && this.type !== "text/plain" ) {
-				droppedFiles.push( this );
+		// Chrome
+		if ( data.items ) {
+			extractData( data.items );
+		} else {
+			if ( !data.files.length ) {
+				// Folder detection for IE
+				alerte( "Your browser doesn't support folders." );
 			} else {
-				saveFile = this;
-				gs.reset();
+				$.each( data && data.files, function() {
+					if ( this.type && this.type !== "text/plain" ) {
+						droppedFiles.push( this );
+					} else if ( !saveFile ) {
+						saveFile = this;
+						gs.reset();
+					}
+				} );
+				gs.load( saveFile )
+				.then( function() {
+					loadFile( droppedFiles );
+				} );
 			}
-		} );
-		gs.load( saveFile )
-		.then( function() {
-			loadFile( droppedFiles );
-		} );
+		}
 		return false;
 	}
 } );
