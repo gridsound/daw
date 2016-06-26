78dd378d7a015e0305cb82d83e23a35292a8a66e
Thomas Tortorini <thomastortorini@gmail.com>
2016-06-26 05:23:21 +0200

    GS: .load simplification

--- gs/load.js
+++ gs/load.js
@@ -1,71 +1,57 @@
-"use strict"
+"use strict";
+
+( function() {
 
 gs.load = function( saveFile ) {
-	var that = this,
-		reader = new FileReader(),
-		ns,
-		nf,
-		p;
-
-	p = new Promise( function( resolve, reject ) {
-		reader.onload = function( event ) {
-			that._save = JSON.parse( event.target.result );
-
-			that.bpm( that._save.bpm );
-
-			that._save.files.forEach( function( f ) {
-				nf = gs.fileCreate( f );
-				nf.samplesToSet = [];
-			} );
-
-			that._save.tracks.forEach( function( t ) {
-				var id = t[ 0 ],
-					isOn = t[ 1 ],
-					name = t[ 2 ];
-
-				while ( id >= ui.tracks.length ) {
-					ui.newTrack();
-				}
-				ui.tracks[ id ].isOn = isOn;
-				ui.tracks[ id ].name = name;
-			} );
-
-			that._save.samples.forEach( function( s ) {
-				var xem = s[ 0 ],
-					trackId = s[ 1 ],
-					fileId = s[ 2 ],
-					when = s[ 3 ],
-					offset = s[ 4 ],
-					duration = s[ 5 ];
-
-				ns = gs.sampleCreate( gs.files[ fileId ] );
-				ns.gsfile.samplesToSet.push(ns);
-
-				// inTrack()
-				ns.track = ui.tracks[ trackId ];
-				ns.track.samples.push( ns );
-				ui.CSS_sampleTrack( ns );
-
-				// moveX
-				ns.xem = xem;
-				ns.savedWhen = when;
-				ns.jqSample.css( "left", when * ui.BPMem + "em" );
-
-				// slip()
-				ns.savedOffset = offset;
-
-				// sample.duration()
-				ns.savedDuration = duration;
-				ns.jqSample.css( "width", duration * ui.BPMem + "em" );
-			} );
-			resolve( that );
-		};
-
+	return new Promise( function( resolve, reject ) {
 		if ( !saveFile ) {
-			resolve( that );
+			resolve();
 		} else {
+			var reader = new FileReader();
+			reader.onload = onload.bind( null, resolve );
 			reader.readAsText( saveFile );
 		}
-	});
-	return p;
+	} );
 };
+
+function onload( resolve, e ) {
+	var save = JSON.parse( e.target.result );
+
+	gs.bpm( save.bpm );
+
+	save.files.forEach( function( f ) {
+		var nf = gs.fileCreate( f );
+		nf.samplesToSet = [];
+	} );
+
+	save.tracks.forEach( function( t ) {
+		var id = t[ 0 ];
+		while ( id >= ui.tracks.length ) {
+			ui.newTrack();
+		}
+		ui.tracks[ id ].isOn = t[ 1 ];
+		ui.tracks[ id ].name = t[ 2 ];
+	} );
+
+	save.samples.forEach( function( s ) {
+		//        0      1       2      3      4        5
+		// s = [ xem, trackId, fileId, when, offset, duration ]
+
+		var ns = gs.sampleCreate( gs.files[ s[ 2 ] ] );
+		ns.gsfile.samplesToSet.push( ns );
+
+		ns.xem = s[ 0 ];
+		ns.savedWhen = s[ 3 ];
+		ns.savedOffset = s[ 4 ];
+		ns.savedDuration = s[ 5 ];
+		ns.track = ui.tracks[ s[ 1 ] ];
+		ns.track.samples.push( ns );
+
+		ui.CSS_sampleTrack( ns );
+		ns.jqSample.css( "left", s[ 3 ] * ui.BPMem + "em" );
+		ns.jqSample.css( "width", s[ 5 ] * ui.BPMem + "em" );
+	} );
+	resolve();
+}
+
+} )();
