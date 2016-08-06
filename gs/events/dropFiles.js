090e103a9505f116db0134c662afae1bad5d7788
Melanie Misty Ducani <melanieducani@gmail.com>
2016-08-06 21:52:42 +0200

    Fix+GS: drop a file after loading a composition doesn't duplicate files anymore

--- gs/events/dropFiles.js
+++ gs/events/dropFiles.js
@@ -2,11 +2,14 @@
 
 ( function() {
 
+var saveFile, arrFiles = [];
+
 document.body.ondragover = function() { return false; };
 document.body.ondrop = function( e ) {
-	var data = e && e.dataTransfer,
-		saveFile = false,
-		droppedFiles = [];
+	var data = e && e.dataTransfer;
+
+	arrFiles = [];
+	saveFile = false;
 
 	// Chrome :
 	if ( data.items ) {
@@ -21,20 +24,18 @@ document.body.ondrop = function( e ) {
 		var f, i = 0;
 		while ( f = data.files[ i++ ] ) {
 			if ( f.type && f.type !== "text/plain" ) {
-				droppedFiles.push( f );
+				arrFiles.push( f );
 			} else if ( !saveFile ) {
 				saveFile = f;
 				gs.reset();
 			}
 		}
 		gs.load( saveFile ).then( function() {
-			loadFiles( droppedFiles );
+			loadFiles();
 		} );
 	}
 	return false;
 };
-
-var saveFile, arrFiles = [];
 
 function traverseTree( item ) {
 	return new Promise( function( resolve ) {
@@ -71,13 +72,13 @@ function extractData( droppedItems ) {
 	}
 	Promise.all( arrayPromises ).then( function() {
 		gs.load( saveFile ).then( function() {
-			loadFiles( arrFiles );
+			loadFiles();
 		} );
 	} );
 }
 
-function loadFiles( droppedFiles ) {
-	droppedFiles.forEach( function( file ) {
+function loadFiles() {
+	arrFiles.forEach( function( file ) {
 		if ( !gs.files.some( function( f ) {
 			var size = f.file ? f.file.size : f.size;
 			if ( f.fullname === file.name && size === file.size ) {
