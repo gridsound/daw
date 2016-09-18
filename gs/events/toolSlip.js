140ff44cd4e58016debde62e5593af2224822c0c
Melanie Misty Ducani <melanieducani@gmail.com>
2016-09-19 00:04:23 +0200

    GS: history: undo/redo action with slip tool is available.

--- gs/events/toolSlip.js
+++ gs/events/toolSlip.js
@@ -2,14 +2,31 @@
 
 ( function() {
 
-var sampleSave;
+var	sampleSave,
+	oldOffset;
 
 ui.tool.slip = {
 	mousedown: function( e ) {
 		sampleSave = e.target.gsSample;
+		if ( sampleSave ) {
+			oldOffset = sampleSave.wsample.offset;
+		}
 	},
 	mouseup: function() {
 		if ( sampleSave ) {
+			gs.history.push( {
+				name: "slip",
+				action: {
+					func: gs.history.slip,
+					sample: sampleSave,
+					offsetDiff: ( oldOffset - sampleSave.wsample.offset )
+				},
+				undo: {
+					func: gs.history.undoSlip,
+					sample: sampleSave,
+					offsetDiff: sampleSave.wsample.offset - oldOffset
+				}
+			} );
 			gs.samplesForEach( sampleSave, function( s ) {
 				wa.composition.update( s.wsample, "mv" );
 			} );
