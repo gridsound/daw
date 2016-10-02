8cfcb09b58a8860f75b11bae9b749d4160416d96
Thomas Tortorini <thomastortorini@gmail.com>
2016-10-03 00:26:50 +0200

    GS: save, beautify the savefile

--- gs/save.js
+++ gs/save.js
@@ -1,45 +1,42 @@
 "use strict"
 
 gs.save = function() {
-	var _save = {
-			bpm: this._bpm,
-			files: [],
-			samples: [],
-			tracks: []
-		};
-
-	gs.files.forEach( function( f ) {
-		_save.files.push( [
-			f.id,
-			f.fullname,
-			f.file ? f.file.size : f.size
-		] );
-	} );
-
-	gs.samples.forEach( function( s ) {
-		_save.samples.push( [
-			s.track.id,
-			s.gsfile.id,
-			s.wsample ? s.wsample.when : s.savedWhen,
-			s.wsample ? s.wsample.offset : s.savedOffset,
-			s.wsample ? s.wsample.duration : s.savedDuration
-		] );
-	} );
-
-	// TODO save wfilters
-	ui.tracks.forEach( function( t ) {
-		if ( !t.isOn || t.samples.length ||
-			 t.name || ( t.wfilters && t.wfilters.length ) ) {
-			_save.tracks.push( [
-				t.id,
-				t.isOn,
-				t.name
-			] );
-		}
-	} );
+	var files = gs.files.map( function( f ) {
+			return [ f.id, f.fullname, f.file ? f.file.size : f.size ];
+		} ),
+		samples = gs.samples.map( function( s ) {
+			return [
+				s.track.id,
+				s.gsfile.id,
+				s.wsample ? s.wsample.when : s.savedWhen,
+				s.wsample ? s.wsample.offset : s.savedOffset,
+				s.wsample ? s.wsample.duration : s.savedDuration
+			];
+		} ),
+		tracks = ui.tracks.reduce( function( arr, t ) {
+			if ( !t.isOn || t.samples.length || t.name || ( t.wfilters && t.wfilters.length ) ) {
+				arr.push( [ t.id, t.isOn, t.name ] );
+			}
+			return arr;
+		}, [] );
 
 	return {
-		href: "data:text/plain;charset=utf-8," + encodeURIComponent( JSON.stringify( _save ) ),
-		download: "s.txt"
+		download: "gridsound-composition.gs",
+		href: "data:text/plain;charset=utf-8," + encodeURIComponent(
+			'{' +
+				'\n\t"saveformat": 1,' +
+				'\n\t"bpm": ' + gs._bpm + ',' +
+				'\n\t"files": [\n'   + reduce( files   ) + '\t],' +
+				'\n\t"samples": [\n' + reduce( samples ) + '\t],' +
+				'\n\t"tracks": [\n'  + reduce( tracks  ) + '\t]'  +
+			'\n}'
+		)
 	};
 };
+
+function reduce( arr ) {
+	var len = arr.length - 1;
+	return arr.reduce( function( _, it, i ) {
+		return _ + "\t\t" + JSON.stringify( it ) + ( i < len ? "," : "" ) + "\n";
+	}, "" )
+}
