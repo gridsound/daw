1fedfff578e6c8461d5c1319c4f5a9408a3fab15
Thomas Tortorini <thomastortorini@gmail.com>
2016-11-13 13:40:49 +0100

    Google Analytics only on production

--- analyticstracking.js
+++ analyticstracking.js
@@ -1,7 +1,9 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+if ( location.host === "gridsound.github.io" ) {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-418864-6', 'auto');
-ga('send', 'pageview');
+	ga('create', 'UA-418864-6', 'auto');
+	ga('send', 'pageview');
+}
