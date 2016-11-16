03460f980360f18c76018464e40b1e4ade88d55c
Thomas Tortorini <thomastortorini@gmail.com>
2016-11-16 15:37:53 +0100

    GS: events, toolDelete, group all the deletion with the toolmouse

--- gs/events/toolDelete.js
+++ gs/events/toolDelete.js
@@ -4,15 +4,23 @@
 
 ui.tool.delete = {
 	mousedown: del,
-	mousemove: del
+	mousemove: del,
+	mouseup: function() {
+		if ( smpDeleted.length ) {
+			gs.history.push( "delete", { samples: smpDeleted.slice() } );
+			smpDeleted.length = 0;
+		}
+	}
 };
+
+var smpDeleted = [];
 
 function del( e ) {
 	var smp = e.target.gsSample;
 
 	if ( smp ) {
-		gs.history.push( "delete", { samples: [ smp ] } );
 		gs.samples.selected.delete( smp );
+		smpDeleted.push( smp );
 	}
 }
 
