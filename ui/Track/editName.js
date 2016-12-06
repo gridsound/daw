7a521ca0b74f848c089fa97c30e6c82602fe4776
Thomas Tortorini <thomastortorini@gmail.com>
2016-12-06 05:50:11 +0100

    UI+fix: gs-ui-span-editable, track.editName

--- ui/Track/editName.js
+++ ui/Track/editName.js
@@ -3,6 +3,7 @@
 ui.Track.prototype.initEditName = function() {
 	var that = this;
 
+	this.name = "";
 	this.elName = this.elColNamesTrack.querySelector( ".gs-ui-span-editable" );
 	gsUIComponents( this.elName, {
 		onchange: function( val ) {
