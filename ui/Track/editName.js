dd1b4b0767de6362fecdec23cfbb3da8e47a48aa
Thomas Tortorini <thomastortorini@gmail.com>
2016-12-07 08:09:40 +0100

    gs-ui-components: update

--- ui/Track/editName.js
+++ ui/Track/editName.js
@@ -4,17 +4,17 @@ ui.Track.prototype.initEditName = function() {
 	var that = this;
 
 	this.name = "";
-	this.elName = this.elColNamesTrack.querySelector( ".gs-ui-span-editable" );
-	gsUIComponents( this.elName, {
+	this.gsuiSpanEditable = new gsuiSpanEditable(
+		this.elColNamesTrack.querySelector( ".gs-ui-span-editable" ), {
 		onchange: function( val ) {
 			that.name = val;
 		}
 	} );
-	this.elName._setPlaceholder( "Track " + ( this.id + 1 ) );
+	this.gsuiSpanEditable.setPlaceholder( "Track " + ( this.id + 1 ) );
 	return this;
 };
 
 ui.Track.prototype.editName = function( name ) {
-	this.elName._setValue( name );
+	this.gsuiSpanEditable.setValue( name );
 	return this;
 };
--- ui/Track/toggle.js
+++ ui/Track/toggle.js
@@ -3,8 +3,8 @@
 ui.Track.prototype.initToggle = function() {
 	var that = this;
 
-	this.elToggle = this.elColNamesTrack.querySelector( ".gs-ui-toggle" );
-	gsUIComponents( this.elToggle, {
+	this.gsuiToggle = new gsuiToggle(
+		this.elColNamesTrack.querySelector( ".gs-ui-toggle" ), {
 		onchange: function( b ) {
 			that.isOn = b;
 			that.wfilters.gain( +b );
@@ -13,13 +13,13 @@ ui.Track.prototype.initToggle = function() {
 		}
 	} );
 	if ( ui.tracks.length ) {
-		this.elToggle._groupWith( ui.tracks[ 0 ].elToggle );
+		this.gsuiToggle.groupWith( ui.tracks[ 0 ].gsuiToggle );
 	}
 	return this;
 };
 
 ui.Track.prototype.toggle = function( b ) {
-	this.elToggle._toggle( b );
+	this.gsuiToggle.toggle( b );
 	return this;
 };
 
