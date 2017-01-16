11ea6bc7d3c994df98da86f06ff1a60f52935fd6
Thomas Tortorini <thomastortorini@gmail.com>
2017-01-16 17:32:14 +0100

    gsuiOscilloscope: update

--- ui/visual.js
+++ ui/visual.js
@@ -1,18 +1,35 @@
 "use strict";
 
 ui.initElement( "visual", function( el ) {
-	var emptyArr = [],
-		uiOsc = new gsuiOscilloscope( el.querySelector( ".gsuiOscilloscope" ) );
+	var uiOsc = new gsuiOscilloscope( el.querySelector( ".gsuiOscilloscope" ) );
 
 	uiOsc.setResolution( 256, el.clientHeight );
 	uiOsc.setPinch( 1 );
 	uiOsc.dataFunction( function() {
-		var data = emptyArr;
-
-		if ( gs.wctx.nbPlaying ) {
-			gs.analyserNode.getByteTimeDomainData( data = gs.analyserData );
+		gs.analyserNode.getByteTimeDomainData( gs.analyserData );
+		return gs.analyserData;
+	} );
+	uiOsc.drawBegin( function( ctx, max, w, h ) {
+		if ( gs.isPaused ) {
+			return false;
 		}
-		return data;
+		if ( !gs.isPlaying ) {
+			uiOsc.clear();
+			return false;
+		}
+		ctx.globalCompositeOperation = "source-in";
+		ctx.fillStyle = "rgba(0,0,0,.8)";
+		ctx.fillRect( 0, 0, w, h );
+		ctx.globalCompositeOperation = "source-over";
+	} );
+	uiOsc.drawEnd( function( ctx, max ) {
+		if ( max < .01 ) {
+			ctx.strokeStyle = "rgba(0,0,0,0)";
+		} else {
+			ctx.strokeStyle = "#52666e";
+			ctx.lineJoin = "round";
+			ctx.lineWidth = 2;
+		}
 	} );
 
 	return {
