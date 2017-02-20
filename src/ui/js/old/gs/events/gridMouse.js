e7bec2d3e3cac065e1da7486301263ae8d276022
Thomas Tortorini <thomastortorini@gmail.com>
2017-02-20 17:26:12 +0100

    gridcomtent: move the mouse event code

--- src/ui/js/old/gs/events/gridMouse.js
+++ src/ui/js/old/gs/events/gridMouse.js
@@ -1,7 +1,5 @@
 "use strict";
 
-( function() {
-
 ui.px_x =
 ui.px_y =
 ui.px_xRel =
@@ -13,27 +11,10 @@ ui.dom.gridCols.onwheel = function( e ) {
 		return false;
 	}
 };
+
 ui.dom.gridCols.onscroll = function() {
 	ui.grid._scrollTop = ui.dom.gridCols.scrollTop;
 	ui.updateGridTopShadow();
-};
-
-ui.dom.gridcontent.oncontextmenu = function() { return false; };
-ui.dom.gridcontent.onmousedown = function( e ) {
-	if ( !mouseIsDown ) {
-		var fn;
-
-		mouseIsDown = true;
-		mousedownSec = ui.grid.getWhen( e.pageX );
-		ui.px_x = e.pageX;
-		ui.px_y = e.pageY;
-		if ( e.button === 2 ) {
-			ui.tools.save();
-			ui.tools.select( "delete" );
-		}
-		fn = ui.tool[ ui.currentTool ].mousedown;
-		fn && fn( e );
-	}
 };
 
 document.body.onwheel = function( e ) {
@@ -43,7 +24,7 @@ document.body.onwheel = function( e ) {
 };
 
 document.body.addEventListener( "mousemove", function( e ) {
-	if ( mouseIsDown ) {
+	if ( ui.mouseIsDown ) {
 		var fn = ui.tool[ ui.currentTool ].mousemove,
 			secNew = ui.grid.getWhen( e.pageX );
 
@@ -52,28 +33,22 @@ document.body.addEventListener( "mousemove", function( e ) {
 		ui.px_x = e.pageX;
 		ui.px_y = e.pageY;
 		if ( fn ) {
-			mousedownSec += fn( e, secNew - mousedownSec );
+			ui.mousedownSec += fn( e, secNew - ui.mousedownSec );
 		}
 	}
 } );
 
 document.body.addEventListener( "mouseup", function( e ) {
-	if ( mouseIsDown ) {
+	if ( ui.mouseIsDown ) {
 		var fn = ui.tool[ ui.currentTool ].mouseup;
 
 		fn && fn( e );
-		setBackOldTool();
+		ui.tools.restore();
+		ui.mouseIsDown = false;
 	}
 } );
 
-window.addEventListener( "blur", setBackOldTool );
-
-var mouseIsDown,
-	mousedownSec;
-
-function setBackOldTool() {
+window.addEventListener( "blur", function() {
 	ui.tools.restore();
-	mouseIsDown = false;
-}
-
-} )();
+	ui.mouseIsDown = false;
+} );
--- src/ui/js/partials/gridcontent.js
+++ src/ui/js/partials/gridcontent.js
@@ -2,6 +2,23 @@
 
 ui.gridcontent = {
 	init: function() {
+		ui.dom.gridcontent.oncontextmenu = function() { return false; };
+		ui.dom.gridcontent.onmousedown = function( e ) {
+			if ( !ui.mouseIsDown ) {
+				var fn;
+
+				ui.mouseIsDown = true;
+				ui.mousedownSec = ui.grid.getWhen( e.pageX );
+				ui.px_x = e.pageX;
+				ui.px_y = e.pageY;
+				if ( e.button === 2 ) {
+					ui.tools.save();
+					ui.tools.select( "delete" );
+				}
+				fn = ui.tool[ ui.currentTool ].mousedown;
+				fn && fn( e );
+			}
+		};
 	},
 	left: function( xpx ) {
 		ui.trackLinesLeft = xpx = Math.min( ~~xpx, 0 );
