3823ee81d73a69ab7146ba6f56c9f4c602864a38
Thomas Tortorini <thomastortorini@gmail.com>
2017-03-09 17:37:31 +0100

    src/ui/js/templates/historyAction

--- src/ui/js/templates/historyAction.js
+++ src/ui/js/templates/historyAction.js
@@ -1,26 +1,16 @@
 "use strict";
 
-( function() {
-
-var typeToIcon = {
-		sampleCreate: "paint",
-		sampleRemove: "delete",
-	};
-
-// "paint cut delete move crop select slip"
-
 ui.historyAction = function( actobj ) {
 	var tpl = document.querySelector( "#historyAction" ).content,
-		elRoot = document.importNode( tpl, true ),
-		icon = typeToIcon[ actobj.type ];
+		elRoot = document.importNode( tpl, true );
 
 	ui.dom.templateCloned.appendChild( elRoot );
 	elRoot = ui.dom.templateCloned.querySelector( ".historyAction" );
 	elRoot.remove();
 
 	this.elRoot = elRoot;
-	elRoot.querySelector( ".type" ).classList.add( icon );
-	elRoot.querySelector( ".title" ).textContent = icon;
+	elRoot.querySelector( ".type" ).classList.add( actobj.type );
+	elRoot.querySelector( ".title" ).textContent = actobj.type;
 	elRoot.querySelector( ".text" ).textContent = actobj.text;
 };
 
@@ -29,5 +19,3 @@ ui.historyAction.prototype = {
 		this.elRoot.classList.toggle( "undone", !b );
 	}
 };
-
-} )();
