4aef55ffbe7dd62add8bebf6eb9d966a547f7233
Thomas Tortorini <thomastortorini@gmail.com>
2017-04-06 03:29:06 +0200

    src/ui/js/partials/controls: fully use waFwk

--- src/ui/js/partials/controls.js
+++ src/ui/js/partials/controls.js
@@ -2,31 +2,18 @@
 
 ui.controls = {
 	init: function() {
-		ui.dom.btnPlay.onclick = gs.playPause;
-		ui.dom.btnStop.onclick = gs.stop;
+		ui.dom.btnStop.onclick = waFwk.stop.bind( waFwk );
+		ui.dom.btnPlay.onclick = function() {
+			waFwk.isPlaying ? waFwk.pause() : waFwk.play();
+			return false;
+		};
 	},
 	play: function() {
 		ui.dom.btnPlay.classList.remove( "play" );
 		ui.dom.btnPlay.classList.add( "pause" );
-		ui.controls._frame();
-	},
-	pause: function() {
-		cancelAnimationFrame( ui.controls._reqFrameId );
-		ui.dom.btnPlay.classList.remove( "pause" );
-		ui.dom.btnPlay.classList.add( "play" );
 	},
 	stop: function() {
-		waFwk.on.pause();
-		ui.timeline.currentTime( 0 );
-		ui.clock.currentTime( 0 );
-	},
-
-	// private:
-	_frame: function() {
-		var sec = gs.composition.currentTime();
-
-		ui.timeline.currentTime( sec );
-		ui.clock.currentTime( sec );
-		ui.controls._reqFrameId = requestAnimationFrame( ui.controls._frame );
+		ui.dom.btnPlay.classList.remove( "pause" );
+		ui.dom.btnPlay.classList.add( "play" );
 	}
 };
