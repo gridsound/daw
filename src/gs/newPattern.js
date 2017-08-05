04a37fe55cfb91fa01de325c225baede80b76420
Thomas 'Mr21' Tortorini <thomastortorini@gmail.com>
2017-08-05 03:09:38 +0200

    gs.newPattern: rewrite simpler

--- src/gs/newPattern.js
+++ src/gs/newPattern.js
@@ -3,20 +3,15 @@
 gs.newPattern = function( pushToHist ) {
 	var keysId = common.uuid(),
 		patId = common.uuid(),
-		patName = "pat " + ++gs.patternIdAbs,
-		obj = {
+		patName = "pat " + ++gs.patternIdAbs;
+
+	( pushToHist === false
+		? gs.changeComposition
+		: gs.pushCompositionChange )( {
+			keys: { [ keysId ]: {} },
 			patterns: {
 				[ patId ]: { name: patName, type: "keys", keys: keysId, duration: 0 }
-			},
-			keys: {
-				[ keysId ]: {}
 			}
-		};
-
-	if ( pushToHist === false ) {
-		gs.changeComposition( obj );
-	} else {
-		gs.pushCompositionChange( obj );
-	}
+		} );
 	return patId;
 };
