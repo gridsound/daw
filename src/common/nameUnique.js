691bc8ccb53770d336febc3def21f4c40fc30994
Thomas 'Mr21' Tortorini <thomastortorini@gmail.com>
2017-08-07 16:02:13 +0200

    common: trim2 + nameUnique

---
+++ src/common/nameUnique.js
@@ -0,0 +1,16 @@
+"use strict";
+
+common.nameUnique = function( name, arr ) {
+	var reg, nb;
+
+	name = common.trim2( name );
+	if ( arr.indexOf( name ) > -1 ) {
+		reg = new RegExp( "^" + name + " \\((\\d+)\\)$" );
+		nb = arr.reduce( function( nb, str ) {
+			res = reg.exec( str );
+			return res ? Math.max( nb, +res[ 1 ] ) : nb;
+		}, 1 );
+		return `${ name } (${ nb + 1 })`;
+	}
+	return name;
+};
---
+++ src/common/trim2.js
@@ -0,0 +1,5 @@
+"use strict";
+
+common.trim2 = function( str ) {
+	return str.trim().replace( /\s+/g, " " );
+};
