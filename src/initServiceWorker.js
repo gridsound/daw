37520e1ee333ba141af0342a31c7dfa355e97f18
Thomas Tortorini <thomastortorini@gmail.com>
2017-09-04 04:47:27 +0200

    initServiceWorker: add a if to let Safari etc. a chance

--- src/initServiceWorker.js
+++ src/initServiceWorker.js
@@ -1,11 +1,11 @@
 "use strict";
 
-navigator.serviceWorker.register( "serviceWorker.js", { scope: "/daw/" } )
-	.then( function( reg ) {
-		console.log( "Service worker " + (
-			reg.installing ? "installing" :
-			reg.waiting ? "installed" :
-			reg.active ? "active" : "" ) );
-	}, function( err ) {
-		console.log( "Service worker registration failed", err );
-	} );
+if ( navigator.serviceWorker ) {
+	navigator.serviceWorker.register( "serviceWorker.js", { scope: "/daw/" } )
+		.then( function( reg ) {
+			console.log( "Service worker " + (
+				reg.installing ? "installing" :
+				reg.waiting ? "installed" :
+				reg.active ? "active" : "" ) );
+		}, console.log.bind( console, "Service worker registration failed" ) );
+}
