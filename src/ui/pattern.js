f6e74e4807c83825e20eb2c97fe759122fd16910
Thomas Tortorini <thomastortorini@gmail.com>
2017-09-11 09:12:23 +0200

    ui.pattern._onkeydown/up(): the keys is now sonore

--- src/ui/pattern.js
+++ src/ui/pattern.js
@@ -7,11 +7,13 @@ ui.pattern = {
 		grid.loadKeys( 3, 5 );
 		grid.offset( 0, 120 );
 		grid.setFontSize( 20 );
+		ui.keysGridSamples = grid;
 		ui.idElements.keysGridWrap.append( grid.rootElement );
 		ui.idElements.patternName.onclick = ui.pattern._onclickPatternName;
 		grid.onchange = ui.pattern._onchangeGrid;
+		grid.uiKeys.onkeydown = ui.pattern._onkeydown;
+		grid.uiKeys.onkeyup = ui.pattern._onkeyup;
 		grid.resized();
-		ui.keysGridSamples = grid;
 	},
 	empty() {
 		ui.pattern.name( "" );
@@ -26,6 +28,12 @@ ui.pattern = {
 	},
 
 	// private:
+	_onkeydown( key, oct ) {
+		wa.synth.simpleStart( ( key + oct ).toUpperCase() );
+	},
+	_onkeyup() {
+		wa.synth.stop();
+	},
 	_onchangeGrid( obj ) {
 		gs.pushCompositionChange( { keys: {
 			[ gs.currCmp.patterns[ gs.currCmp.patternOpened ].keys ]: obj
