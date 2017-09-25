f6f76ae80f88ba7640e05339d96d991fd6fea0cc
Thomas Tortorini <thomastortorini@gmail.com>
2017-09-25 04:05:05 +0200

    gs.exportCompositionJSON: beautify code

--- src/gs/exportCompositionJSON.js
+++ src/gs/exportCompositionJSON.js
@@ -2,7 +2,7 @@
 
 gs.exportCompositionJSON = function( id ) {
 	var cmp = id === gs.currCmp.id ? gs.currCmp : gs.localStorage.get( id ),
-		blob = new Blob( [ JSON.stringify( cmp ) ] );
+		blob = new Blob( [ JSON.stringify( cmp, null, '\t' ) ] );
 
 	ui.downloadBlob( ( cmp.name || "untitled" ) + ".gs", blob );
 };
