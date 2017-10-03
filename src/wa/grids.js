8301c32114664696d70aa2a6b0712dd5272e0d8d
Melanie Misty Ducani <melanieducani@gmail.com>
2017-10-03 03:33:02 +0200

    wa.playMain()

--- src/wa/grids.js
+++ src/wa/grids.js
@@ -7,7 +7,27 @@ wa.grids = {
 		}
 	},
 	playMain() {
-		console.log( "wa.grids.main.play()" );
+		var cmp = gs.currCmp,
+			synth = new gswaSynth(),
+			sched = new gswaScheduler();
+
+		wa._synth = synth;
+		wa._scheduler = sched;
+		synth.setContext( wa.ctx );
+		synth.connect( wa.ctx.destination );
+		synth.change( { oscillators: {
+			"osc1": { type: "sine", detune: 0 }
+		} } );
+		sched.setContext( wa.ctx );
+		sched.setData( wa.blocksToScheduleData() );
+		sched.setBPM( cmp.bpm );
+		sched.onstart = function( smp, when, offset, duration ) {
+			synth.start( smp.key, wa.ctx.currentTime + when, offset, duration );
+		};
+		sched.onended = function( data ) {
+			gs.controls.stop();
+		};
+		sched.start();
 	},
 	playPattern( id ) {
 		var cmp = gs.currCmp,
