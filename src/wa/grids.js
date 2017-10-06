eeab150a3398f1cd2bf86e84f6c0a6838947073d
Melanie Misty Ducani <melanieducani@gmail.com>
2017-10-06 02:13:22 +0200

    wa.grids._play() : code factorisation on playPattern and playMain

--- src/wa/grids.js
+++ src/wa/grids.js
@@ -7,29 +7,23 @@ wa.grids = {
 		}
 	},
 	playMain() {
-		var cmp = gs.currCmp,
-			synth = new gswaSynth(),
-			sched = new gswaScheduler();
-
-		wa._synth = synth;
-		wa._scheduler = sched;
-		synth.setContext( wa.ctx );
-		synth.connect( wa.ctx.destination );
-		synth.change( { oscillators: {
-			"osc1": { type: "sine", detune: 0 }
-		} } );
-		sched.setContext( wa.ctx );
-		sched.setData( wa.blocksToScheduleData() );
-		sched.setBPM( cmp.bpm );
-		sched.onstart = function( smp, when, offset, duration ) {
-			synth.start( smp.key, wa.ctx.currentTime + when, offset, duration );
-		};
-		sched.onended = function( data ) {
-			gs.controls.stop();
-		};
-		sched.start();
+		wa.grids._play()
 	},
 	playPattern( id ) {
+		wa.grids._play( id )
+	},
+	stop() {
+		if ( wa._synth ) {
+			delete wa._scheduler.onended;
+			wa._scheduler.stop();
+			wa._synth.stop();
+			delete wa._synth;
+			delete wa._scheduler;
+		}
+	},
+
+	// private:
+	_play( id ) {
 		var cmp = gs.currCmp,
 			pat = cmp.patterns[ id ],
 			synth = new gswaSynth(),
@@ -43,7 +37,9 @@ wa.grids = {
 			"osc1": { type: "sine", detune: 0 }
 		} } );
 		sched.setContext( wa.ctx );
-		sched.setData( wa.keysToScheduleData( cmp.keys[ pat.keys ], 0, 0, pat.duration ) );
+		sched.setData( pat
+			? wa.keysToScheduleData( cmp.keys[ pat.keys ], 0, 0, pat.duration )
+			: wa.blocksToScheduleData() );
 		sched.setBPM( cmp.bpm );
 		sched.onstart = function( smp, when, offset, duration ) {
 			synth.start( smp.key, wa.ctx.currentTime + when, offset, duration );
@@ -52,14 +48,5 @@ wa.grids = {
 			gs.controls.stop();
 		};
 		sched.start();
-	},
-	stop() {
-		if ( wa._synth ) {
-			delete wa._scheduler.onended;
-			wa._scheduler.stop();
-			wa._synth.stop();
-			delete wa._synth;
-			delete wa._scheduler;
-		}
 	}
 };
