5b3922441d5c5742e3f6a5cac3a11b2598820985
Thomas Tortorini <thomastortorini@gmail.com>
2017-10-14 05:18:37 +0200

    common.nameUnique(): handle when the name already contains '(\d+)'

--- src/common/nameUnique.js
+++ src/common/nameUnique.js
@@ -5,6 +5,9 @@ common.nameUnique = function( name, arr ) {
 
 	name = common.trim2( name );
 	if ( arr.indexOf( name ) > -1 ) {
+		if ( /\(\d+\)$/.test( name ) ) {
+			name = name.substr( 0, name.lastIndexOf( "(" ) ).trim();
+		}
 		reg = new RegExp( "^" + name + " \\((\\d+)\\)$" );
 		nb = arr.reduce( function( nb, str ) {
 			res = reg.exec( str );
