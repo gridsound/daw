b595e33d62375ae455b0e42d0795d1249b9f72d3
Thomas Tortorini <thomastortorini@gmail.com>
2017-10-16 15:34:22 +0200

    HTML: version -> changelog

--- src/checkNewVersion.js
+++ src/checkNewVersion.js
@@ -4,11 +4,6 @@ setTimeout( function() {
 	fetch( "https://gridsound.github.io/daw/VERSION?" + Math.random() )
 		.then( res => res.text() )
 		.then( res => {
-			var status = res === env.version ? "ok" : "ko";
-
-			dom.version.classList.add( status );
-			dom.version.title = status === "ok"
-				? "GridSound is up to date :)"
-				: "Clear your cache for the version " + res;
+			dom.version.classList.add( res === env.version ? "ok" : "ko" );
 		} );
 }, 2000 );
