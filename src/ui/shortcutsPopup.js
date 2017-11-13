0d69c26700b5815ba4aa0900bd539de2af7e813c
Timer91 <marais.jeremie@gmail.com>
2017-11-13 23:32:25 +0100

    comments deleted

--- src/ui/shortcutsPopup.js
+++ src/ui/shortcutsPopup.js
@@ -6,13 +6,7 @@ ui.shortcutsPopup = {
 	},
 	show() {
 		var cmp = gs.currCmp;
-		// 	inp = ui.shortcutsPopup.inputs;
 
-		// inp[ +env.clockSteps ].checked = true;
-		// inp[ 2 ].value = cmp.name;
-		// inp[ 3 ].value = cmp.bpm;
-		// inp[ 4 ].value = cmp.beatsPerMeasure;
-		// inp[ 5 ].value = cmp.stepsPerBeat;
 		gsuiPopup.custom( "Shortcuts", dom.shortcutsPopupContent );
 		return false;
 	}
