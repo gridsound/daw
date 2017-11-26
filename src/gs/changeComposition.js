6b8409553b00f67ba527d247615e0556e81347ea
Thomas Tortorini <thomastortorini@gmail.com>
2017-11-26 07:35:56 +0100

    gs.changeComposition(): more functional

--- src/gs/changeComposition.js
+++ src/gs/changeComposition.js
@@ -1,50 +1,41 @@
 "use strict";
 
 gs.changeComposition = function( obj ) {
-	var keysId, patId, blcId, synthId,
-		blc,
-		cmp = gs.currCmp,
-		dur = 0,
-		currDur = cmp.duration,
-		bPM = obj.beatsPerMeasure,
-		sPB = obj.stepsPerBeat;
+	var cmp = gs.currCmp,
+		currDur = cmp.duration;
 
 	common.assignDeep( cmp, obj );
 	ui.mainGrid.change( obj );
 	if ( obj.synths ) {
 		ui.synth.change( obj.synths[ cmp.synthOpened ] );
 	}
-	for ( patId in obj.patterns ) {
-		ui.patterns.change( patId, obj.patterns[ patId ] );
-		wa.patterns.change( patId, obj.patterns[ patId ] );
-	}
-	for ( keysId in obj.keys ) {
-		for ( patId in cmp.patterns ) {
-			if ( cmp.patterns[ patId ].keys === keysId ) {
+	obj.patterns && Object.entries( obj.patterns ).forEach( function( [ id, obj ] ) {
+		ui.patterns.change( id, obj );
+		wa.patterns.change( id, obj );
+	} );
+	obj.keys && Object.entries( obj.keys ).forEach( function( [ keysId, keysObj ] ) {
+		Object.entries( cmp.patterns ).some( function( [ patId, pat ] ) {
+			if ( pat.keys === keysId ) {
 				gs.updatePatternContent( patId );
 				if ( patId === cmp.patternOpened ) {
-					ui.keysGridSamples.change( obj.keys[ keysId ] );
+					ui.keysGridSamples.change( keysObj );
 				}
-				break;
+				return true;
 			}
-		}
-	}
+		} );
+	} );
 	if ( obj.bpm ) {
 		ui.controls.bpm( obj.bpm );
 	}
-	if ( bPM || sPB ) {
+	if ( obj.beatsPerMeasure || obj.stepsPerBeat ) {
 		ui.mainGridSamples.timeSignature( cmp.beatsPerMeasure, cmp.stepsPerBeat );
 		ui.keysGridSamples.timeSignature( cmp.beatsPerMeasure, cmp.stepsPerBeat );
-		for ( patId in cmp.patterns ) {
-			gs.updatePatternContent( patId );
-		}
+		Object.keys( cmp.patterns ).forEach( gs.updatePatternContent );
 	}
-	for ( blcId in cmp.blocks ) {
-		blc = cmp.blocks[ blcId ];
-		dur = Math.max( dur, blc.when + blc.duration );
-	}
-	cmp.duration = dur;
-	if ( obj.name != null || obj.bpm || Math.ceil( dur ) !== Math.ceil( currDur ) ) {
+	cmp.duration = Object.values( cmp.blocks ).reduce( function( dur, blc ) {
+		return Math.max( dur, blc.when + blc.duration );
+	}, 0 );
+	if ( obj.name != null || obj.bpm || Math.ceil( cmp.duration ) !== Math.ceil( currDur ) ) {
 		ui.cmps.update( cmp.id, cmp );
 	}
 	wa.grids.replay();
