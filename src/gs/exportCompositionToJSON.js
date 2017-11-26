342abd8754f00c1f5c890b6bcab9fefe666e3d23
Thomas Tortorini <thomastortorini@gmail.com>
2017-11-26 05:47:27 +0100

    gs.exportCompositionToJSON(): clever beautify algorythm

--- src/gs/exportCompositionToJSON.js
+++ src/gs/exportCompositionToJSON.js
@@ -1,15 +1,32 @@
 "use strict";
 
 gs.exportCompositionToJSON = function( cmp ) {
-	var del = '\t"keys": {',
-		arr = JSON.stringify( cmp, null, "\t" ).split( del ),
-		a = arr[ 0 ],
-		b = arr[ 1 ];
+	var res,
+		regTab,
+		regTa2,
+		delTabCurr,
+		delTabs = {
+			tracks: 3,
+			patterns: 3,
+			blocks: 3,
+			keys: 4,
+			synths: 5
+		},
+		reg = /^\t"(\w*)": {$/,
+		lines = JSON.stringify( cmp, null, "\t" ).split( "\n" );
 
 	gs._blobDL && URL.revokeObjectURL( gs._blobDL );
-	a = a.replace( /\n\t{3}/g, " " );
-	a = a.replace( /\n\t{2}}/g, " }" );
-	b = b.replace( /\n\t{4}/g, " " );
-	b = b.replace( /\n\t{3}}/g, " }" );
-	return gs._blobDL = URL.createObjectURL( new Blob( [ a + del + b ] ) );
+	lines.forEach( ( line, i ) => {
+		if ( res = reg.exec( line ) ) {
+			if ( delTabCurr = delTabs[ res[ 1 ] ] ) {
+				regTab = new RegExp( `^\\t{${ delTabCurr }}` );
+				regTa2 = new RegExp( `^\\t{${ delTabCurr - 1 }}\\}` );
+			}
+		}
+		if ( delTabCurr ) {
+			lines[ i ] = lines[ i ].replace( regTab, "~" ).replace( regTa2, "~}" );
+		}
+	} );
+	return gs._blobDL = URL.createObjectURL( new Blob( [
+		lines.join( "\n" ).replace( /\n~/g, " " ) ] ) );
 };
