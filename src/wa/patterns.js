b9bf92f1efd88130a1f2e423f154320002173870
Thomas Tortorini <thomastortorini@gmail.com>
2017-11-26 08:36:00 +0100

    wa.patterns: a pattern preview will play with the correct synth

--- src/wa/patterns.js
+++ src/wa/patterns.js
@@ -2,21 +2,28 @@
 
 wa.patterns = {
 	stop() {
-		wa.synth.stop();
+		if ( wa.patterns._synth ) {
+			wa.patterns._synth.stop();
+			delete wa.patterns._synth;
+		}
 	},
 	play( id ) {
-		var idKey, key,
-			cmp = gs.currCmp,
-			keys = cmp.keys[ cmp.patterns[ id ].keys ],
-			bps = cmp.bpm / 60;
+		var cmp = gs.currCmp,
+			bps = cmp.bpm / 60,
+			pat = cmp.patterns[ id ],
+			keys = cmp.keys[ pat.keys ],
+			synth = cmp.synths[ pat.synth ],
+			waSynth = new gswaSynth();
 
-		for ( idKey in keys ) {
-			key = keys[ idKey ];
-			wa.synth.start(
+		waSynth.setContext( wa.ctx );
+		waSynth.connect( wa.destination.get() );
+		waSynth.change( synth );
+		Object.values( keys ).forEach( key => {
+			wa.patterns._synth = waSynth;
+			waSynth.start(
 				key.key.toUpperCase(),
 				wa.ctx.currentTime + key.when / bps,
-				0,
-				key.duration / bps );
-		}
+				0, key.duration / bps );
+		} );
 	}
 };
