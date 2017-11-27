dca17efc65de75d6de37087b4445037b13a6f3ff
Thomas Tortorini <thomastortorini@gmail.com>
2017-11-27 06:44:44 +0100

    wa.init(): remove the old global wasynth

--- src/wa/init.js
+++ src/wa/init.js
@@ -3,14 +3,6 @@
 wa.init = function() {
 	gswaSynth.assignDeep = common.assignDeep;
 	wa.ctx = new AudioContext();
-	wa.synth = new gswaSynth();
 	wa.destination.init( wa.ctx );
 	wa.synths.init();
-	wa.synth.setContext( wa.ctx );
-	wa.synth.connect( wa.ctx.destination );
-	wa.synth.change( {
-		oscillators: {
-			"osc1": { type: "sine", detune: 0, pan: 0, gain: 1 }
-		}
-	} );
 };
