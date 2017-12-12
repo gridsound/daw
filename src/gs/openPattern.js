067a82da56ba92bfe019dc69a525e4ed7546ae84
Thomas Tortorini <thomastortorini@gmail.com>
2017-12-12 05:56:17 +0100

    gs.openPattern(): fix, check on pattern instead id (we can delete two patterns at the same moment)

--- src/gs/openPattern.js
+++ src/gs/openPattern.js
@@ -1,21 +1,18 @@
 "use strict";
 
 gs.openPattern = function( id ) {
-	var synthId,
-		cmp = gs.currCmp;
+	var cmp = gs.currCmp,
+		pat = cmp.patterns[ id ];
 
-	id = id || null;
-	cmp.patternOpened = id;
-	if ( id ) {
-		synthId = cmp.patterns[ id ].synth;
-		gs.controls.focusOn( "pattern" );
+	cmp.patternOpened =
+	id = pat ? id : null;
+	gs.controls.focusOn( pat ? "pattern" : "main" );
+	if ( pat ) {
 		wa.grids.replay();
-		wa.synths.select( synthId );
-		if ( synthId !== cmp.synthOpened ) {
-			gs.openSynth( synthId );
+		wa.synths.select( pat.synth );
+		if ( pat.synth !== cmp.synthOpened ) {
+			gs.openSynth( pat.synth );
 		}
-	} else {
-		gs.controls.focusOn( "main" );
 	}
 	ui.patterns.select( id );
 	ui.pattern.open( id );
