75b2fe671ba9ef2ddd16dd8857ce44007ad2f9df
Thomas Tortorini <thomastortorini@gmail.com>
2018-07-31 22:30:22 +0200

    jsonActions.clonePattern: fix, id string->number

--- src/jsonActions/clonePattern.js
+++ src/jsonActions/clonePattern.js
@@ -1,13 +1,13 @@
 "use strict";
 
-jsonActions.clonePattern = function( patId ) {
+jsonActions.clonePattern = patId => {
 	var nKeys = {},
 		nkeysId = common.smallId(),
 		pat = gs.currCmp.patterns[ patId ],
 		keys = gs.currCmp.keys[ pat.keys ];
 
-	Object.keys( keys ).forEach( function( k ) {
-		nKeys[ common.smallId() ] = Object.assign( {}, keys[ k ] );
+	Object.keys( keys ).forEach( ( k, i ) => {
+		nKeys[ i ] = Object.assign( {}, keys[ k ] );
 	} );
 	return {
 		keys: { [ nkeysId ]: nKeys },
