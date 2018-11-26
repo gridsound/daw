d5ca36c81d5cb3131380f20f88d8841a61724f65
Thomas Tortorini <thomastortorini@gmail.com>
2018-11-26 01:36:18 +0100

    Design: increase the root variables

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ declare -a CSSfiles=(
 	"gs-ui-components/gsuiSliderGroup/gsuiSliderGroup.css"
 	"gs-ui-components/gsuiTimeline/gsuiTimeline.css"
 	"gs-ui-components/gsuiTrack/gsuiTrack.css"
-	"gs-ui-components/gsuiTracklist/gsuiTracklist.css"
 	"src/css/root.css"
 	"src/css/fonts.css"
 	"src/css/icon.css"
