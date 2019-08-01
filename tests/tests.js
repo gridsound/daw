b013626093d907a9c781f4c6066dd36cc16e77e8
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-01 04:10:22 +0200

    Tests: add a utils function, clickX

--- tests/tests.js
+++ tests/tests.js
@@ -7,7 +7,17 @@ QUnit.module( "DAW", () => {
 
 	const qS = document.querySelector.bind( document );
 	const qSA = document.querySelectorAll.bind( document );
+	const clickX = ( el, type, clientX ) => {
+		el.dispatchEvent( new MouseEvent( type, {
+			view: window,
+			bubbles: true,
+			cancelable: true,
+			clientX,
+		} ) );
+	};
 
+	// .............................................................................................
+	// .............................................................................................
 	QUnit.test( "Splashscreen: click", assert => {
 		const done = assert.async();
 
@@ -19,21 +29,18 @@ QUnit.module( "DAW", () => {
 		}, 3000 );
 	} );
 
+	// .............................................................................................
+	// .............................................................................................
 	QUnit.test( "Pianoroll: create a key", assert => {
 		const el = qS( ".gsuiKey-row[ data-midi='57' ]" );
-		const clientX = el.getBoundingClientRect().x + 90; // 1.
-		const ev = new MouseEvent( "mousedown", {
-			view: window,
-			bubbles: true,
-			cancelable: true,
-			clientX,
-		} );
+		const clientX = el.getBoundingClientRect().x + 90;
+		const nbActions = qSA( ".historyAction" ).length;
 
-		el.dispatchEvent( ev );
+		clickX( el, "mousedown", clientX );
 		assert.expect( 3 );
 		assert.strictEqual( document.title, "*GridSound", "document.title" );
 		assert.strictEqual( DAW.get.keys( 0 )[ 1 ].when, 1, "DAW.get.keys( 0 )[ 1 ].when" );
-		assert.strictEqual( qSA( ".historyAction" ).length, 1, '$( ".historyAction" ).length' );
+		assert.strictEqual( qSA( ".historyAction" ).length, nbActions + 1, '$( ".historyAction" ).length' );
 	} );
 } );
 
