61e3d3a7df3a3e14ad83362cda50d89a78d4ae6c
mr21 <thomastortorini@gmail.com>
2021-12-03 07:09:20 +0100

    html/windows/effects: rm the warning beta message

--- src/html/windows/effects.html.js
+++ src/html/windows/effects.html.js
@@ -5,6 +5,5 @@ GSUI.setTemplate( "window-effects", () => (
 		GSUI.createElement( "div", { class: "windowMenu" },
 			GSUI.createElement( "button", { id: "channelName", class: "windowBtn windowDataBtn" } ),
 		),
-		GSUI.createElement( "div", { class: "windowBetaMsg" }, "Beta: the effects may not work as expected due to browser support" ),
 	)
 ) );
