7a79b3a75ca8be952943e8cf6faf24a69630fd87
mr21 <thomastortorini@gmail.com>
2022-04-03 04:52:03 +0200

    0.36.1 remove the hash cmp variable to load automatically something

--- src/run.js
+++ src/run.js
@@ -1,55 +1,33 @@
 "use strict";
 
-UIloading().then( UIrun ).then( UIloaded );
+document.addEventListener( "gsuiEvents", ( { detail: d } ) => {
+	console.warn( `uncatched gsuiEvent: [${ d.component }][${ d.eventName }]`, d.args );
+} );
 
-function UIloading() {
-	return new Promise( resolve => {
-		const el = document.querySelector( "#splashScreen" ),
-			elTitle = document.querySelector( "#splashScreen-title" ),
-			glitch = new TextGlitch( elTitle );
+new Promise( resolve => {
+	const el = document.querySelector( "#splashScreen" ),
+		elTitle = document.querySelector( "#splashScreen-title" ),
+		glitch = new TextGlitch( elTitle );
 
-		el.classList.add( "loaded" );
-		if ( window.CSS && CSS.supports( "clip-path: inset(0 1px 2px 3px)" ) ) {
-			glitch.on();
-		}
-		el.onclick = () => {
-			glitch.off();
-			el.classList.add( "starting" );
-			setTimeout( resolve, 100 );
-		};
-	} );
-}
-
-function UIloaded() {
-	const el = document.querySelector( "#splashScreen" );
-
-	el.classList.add( "started" );
-	setTimeout( () => el.remove(), 800 );
-}
-
-function UIrun() {
-	const daw = new GSDAW();
-	const hash = new Map( location.hash
-		.substr( 1 )
-		.split( "&" )
-		.map( kv => kv.split( "=" ) )
-	);
-
-	daw.getDAWCore().addCompositionsFromLocalStorage();
-	document.addEventListener( "gsuiEvents", e => {
-		const { component, eventName, args } = e.detail;
-
-		console.warn( `uncatched gsuiEvent: [${ component }][${ eventName }]`, args );
-	} );
-	if ( !hash.has( "cmp" ) ) {
-		daw.newComposition();
-	} else {
-		daw.getDAWCore().addCompositionByURL( hash.get( "cmp" ) )
-			.catch( e => {
-				console.error( e );
-				return daw.getDAWCore().addNewComposition();
-			} )
-			.then( cmp => daw.getDAWCore().openComposition( "local", cmp.id ) );
-		location.hash = "";
+	el.classList.add( "loaded" );
+	if ( window.CSS && CSS.supports( "clip-path: inset(0 1px 2px 3px)" ) ) {
+		glitch.on();
 	}
-}
+	el.onclick = () => {
+		glitch.off();
+		el.classList.add( "starting" );
+		setTimeout( resolve, 100 );
+	};
+} )
+	.then( () => {
+		const daw = new GSDAW();
+
+		daw.getDAWCore().addCompositionsFromLocalStorage();
+		daw.newComposition();
+	} )
+	.then( () => {
+		const el = document.querySelector( "#splashScreen" );
+
+		el.classList.add( "started" );
+		setTimeout( () => el.remove(), 800 );
+	} );
