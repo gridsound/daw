d11271c6db5f1305450c9b523994acf6ab6b005b
mr21 <thomastortorini@gmail.com>
2023-02-06 04:38:10 +0100

    0.40.0

--- src/run.js
+++ src/run.js
@@ -22,7 +22,7 @@ new Promise( resolve => {
 	.then( () => {
 		const daw = new GSDAW();
 
-		GSUI.$setAttribute( daw.rootElement, "version", "0.39.2" );
+		GSUI.$setAttribute( daw.rootElement, "version", "0.40.0" );
 		daw.getDAWCore().$importCompositionsFromLocalStorage();
 		daw.newComposition();
 	} )
