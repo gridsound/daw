f9f5919c6c009d94b2d2c188801a27407bf373f9
mr21 <thomastortorini@gmail.com>
2025-01-25 15:28:52 +0100

    build.sh: update count function

--- build.sh
+++ build.sh
@@ -471,15 +471,16 @@ updateDep() {
 }
 
 count() {
-	find ../daw/src/          -name '*.js'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* daw              %4.0f JS lines\n", s}'
-	find ../daw/src/          -name '*.css'                       -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %4.0f CSS lines\n", s}'
-	find ../daw/src/          -name '*.js' -not -name '*.html.js' -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %4.0f HTML lines\n\n", s}'
-	find ../daw-core/         -name '*.js'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* daw-core         %4.0f JS lines\n\n", s}'
-	find ../gs-components/    -name '*.js'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-component     %4.0f JS lines\n\n", s}'
-	find ../gs-wa-components/ -name '*.js'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-wa-components %4.0f JS lines\n\n", s}'
-	find ../gs-ui-components/ -name '*.js' -not -name '*.html.js' -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-ui-components %4.0f JS lines\n", s}'
-	find ../gs-ui-components/ -name '*.css'                       -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %4.0f CSS lines\n", s}'
-	find ../gs-ui-components/ -name '*.html.js'                   -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %4.0f HTML lines\n", s}'
+	find ../daw/src/          -name '*.js' -not -name '*.html.js'  -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* daw              %5.0f JS lines\n", s}'
+	find ../daw/src/          -name '*.css'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %5.0f CSS lines\n", s}'
+	find ../daw/src/          -name '*.html.js' -or -name '*.html' -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %5.0f HTML lines\n\n", s}'
+	find ../daw-core/         -name '*.js'                         -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* daw-core         %5.0f JS lines\n\n", s}'
+	find ../gs-utils/         -name '*.js'                         -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-utils         %5.0f JS lines\n\n", s}'
+	find ../gs-components/    -name '*.js'                         -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-component     %5.0f JS lines\n\n", s}'
+	find ../gs-wa-components/ -name '*.js'                         -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-wa-components %5.0f JS lines\n\n", s}'
+	find ../gs-ui-components/ -name '*.js' -not -name '*.html.js'  -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "* gs-ui-components %5.0f JS lines\n", s}'
+	find ../gs-ui-components/ -name '*.css'                        -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %5.0f CSS lines\n", s}'
+	find ../gs-ui-components/ -name '*.html.js'                    -exec wc -l {} \; | cut -d' ' -f1 | awk '{s+=$1} END {printf "                   %5.0f HTML lines\n", s}'
 }
 
 if [ $# = 0 ]; then
