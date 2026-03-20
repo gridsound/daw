87086241b349266619634716579f5aa193c361d4
mr21 <thomastortorini@gmail.com>
2026-03-20 06:26:42 +0100

    1.57.1

--- src/run.js
+++ src/run.js
@@ -3,34 +3,33 @@
 document.addEventListener( "gsui", ( { detail: d } ) => console.warn( `uncatched event: "${ d.$event }"`, d.$args, d.$target ) );
 
 new Promise( resolve => {
-	const el = GSUdomQS( "#splashScreen" );
-	const elTitle = GSUdomQS( "#splashScreen-title" );
-	const elStart = GSUdomQS( "#splashScreen-start" );
-	const elFirefox = GSUdomQS( "#splashScreen-firefox" );
+	const el = $( "#splashScreen" );
+	const elTitle = $( "#splashScreen-title" );
+	const elStart = $( "#splashScreen-start" );
+	const elFirefox = $( "#splashScreen-firefox" );
 
 	GSUonFirefox
-		? GSUdomStyle( elFirefox, "display", "block" )
-		: elFirefox.remove();
-	GSUdomSetAttr( el, "data-loaded" );
-	GSUdomSetAttr( GSUdomQS( "#splashScreen-logo" ), "data-ready" );
+		? elFirefox.$css( "display", "block" )
+		: elFirefox.$remove();
+	el.$addAttr( "data-loaded" );
+	$( "#splashScreen-logo" ).$addAttr( "data-ready" );
 	if ( window.CSS && CSS.supports( "clip-path: inset(0 1px 2px 3px)" ) ) {
-		GSUdomSetAttr( elTitle, "enable" );
+		elTitle.$addAttr( "enable" );
 	}
-	elStart.onclick = () => {
-		GSUdomRmAttr( elTitle, "enable" );
-		GSUdomSetAttr( el, "data-starting" );
+	elStart.$on( "click", () => {
+		elTitle.$rmAttr( "enable" );
+		el.$addAttr( "data-starting" );
 		GSUsetTimeout( resolve, .1 );
-	};
+	} ).$rmAttr( "disabled" );
 	localStorage.removeItem( "cookieAccepted" );
-	GSUdomRmAttr( elStart, "disabled" );
 } )
 	.then( () => GSUloadJSFile( "assets/gsuiLibrarySamples-v1.js" ) )
 	.then( () => GSUloadJSFile( "assets/gsuiWaveletList-v1.js" ) )
 	.then( () => {
 		const daw = new GSDAW();
-		const el = GSUdomQS( "#splashScreen" );
+		const el = $( "#splashScreen" );
 
 		// window.$daw = daw.getDAWCore();
-		GSUdomSetAttr( el, "data-started" );
-		GSUsetTimeout( () => el.remove(), .8 );
+		el.$addAttr( "data-started" );
+		GSUsetTimeout( () => el.$remove(), .8 );
 	} );
